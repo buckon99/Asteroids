f8c9fbcdde8c9d21a7cb98c9576ff25f20e358d1
jtan26 <julian.k.tan04@gmail.com>
2016-12-09 15:51:20 -0800

    added lasers
    yes

---
+++ js/lasers.js
@@ -0,0 +1,52 @@
+var cX, cY, shoot, x1, y1, speed;
+
+var laser = []
+function setup(){
+	shoot = false;
+	createCanvas(400, 400);
+	//x = 200;
+	//y = 200;
+	cX = width/2;
+	cY = height/2;
+	speed = 0;
+
+}
+function draw(){
+	background(255);
+	fill(255, 0 , 0);
+	
+	var ship = createVector(mouseX-cX, mouseY-cY);
+	ship.normalize();
+	ship.mult(10);
+	line(cX, cY, cX+ship.x, cY+ship.y);
+	
+	for(var i=0; i<laser.length; i++){
+		laser[i].draw();
+		laser[i].updatePos();
+	}
+}
+
+function ddrawLaser(x0, x1, y0, y1){
+		var laser = createVector(x1-x0, y1-y0);
+		laser.normalize();
+		laser.mult(10);
+
+		this.x = x0;
+		this.y = y0;
+		
+	this.draw = function(){
+		push();
+			noStroke();
+			ellipse(this.x, this.y, 5);
+		pop();
+	}
+	this.updatePos = function(){
+		this.x += laser.x;
+		this.y += laser.y;
+	}
+}
+function mouseClicked(){
+
+	laser.push(new drawLaser(cX, mouseX, cY, mouseY));
+
+}
