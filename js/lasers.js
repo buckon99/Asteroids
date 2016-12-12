93ad0a420b971d65bb72280186ebcee4ac24a370
jtan26 <julian.k.tan04@gmail.com>
2016-12-11 23:22:17 -0800

    lasers
    add new lasers

--- js/lasers.js
+++ js/lasers.js
@@ -1,22 +1,29 @@
-var cX;
-var cY;
-var shoot = false;
-var lasers = [];
-function drawLaser(){
+var cX, cY, shoot, x1, y1, speed;
+
+var laser = []
+function setup(){
+	shoot = false;
+	createCanvas(400, 400);
 	cX = width/2;
 	cY = height/2;
+	speed = 0;
+
+}
+function draw(){
+	background(255);
 	fill(255, 0 , 0);
+	
 	var ship = createVector(mouseX-cX, mouseY-cY);
 	ship.normalize();
-	ship.mult(100);
+	ship.mult(10);
 	line(cX, cY, cX+ship.x, cY+ship.y);
-	for(var i=0; i< lasers.length; i++){
-		lasers[i].draw();
-		lasers[i].updatePos();
+	
+	for(var i=0; i<laser.length; i++){
+		laser[i].draw();
+		laser[i].updatePos();
 	}
 }
 
-function Laser(x0, x1, y0, y1){
 		var laser = createVector(x1-x0, y1-y0);
 		laser.normalize();
 		laser.mult(10);
@@ -35,8 +42,72 @@ function Laser(x0, x1, y0, y1){
 		this.y += laser.y;
 	}
 }
-function laserClick(){
 
-	lasers.push(new Laser(cX, mouseX, cY, mouseY));
+function drawBigLaser(x0, x1, y0, y1){ //big laser, can also be used in mouse dragged
+		var laser = createVector(x1-x0, y1-y0);
+		laser.normalize();
+		laser.mult(3);
+
+		this.x = x0;
+		this.y = y0;
+		
+	this.draw = function(){
+		push();
+			noStroke();
+			ellipse(this.x, this.y, 50);
+		pop();
+	}
+	this.updatePos = function(){
+		this.x += laser.x;
+		this.y += laser.y;
+	}
+}
+
+function drawLaserBeam(x0, x1, y0, y1){ //beams
+		var laser = createVector(x1-x0, y1-y0);
+		laser.normalize();
+		laser.mult(5);
+
+		this.x = x0;
+		this.y = y0;
+		
+	this.draw = function(){
+		push();
+			noStroke();
+			for(var i=0; i<10; i+= .5){
+				ellipse(this.x + laser.x*i, this.y + laser.y*i, 5);
+			}
+		pop();
+	}
+	this.updatePos = function(){
+		this.x += laser.x;
+		this.y += laser.y;
+	}
+}
+
+function drawRocket(x0, x1, y0, y1){ //rockets
+		var laser = createVector(x1-x0, y1-y0);
+		laser.normalize();
+		laser.mult(5);
+
+		this.x = x0;
+		this.y = y0;
+		
+	this.draw = function(){
+		push();
+			noStroke();
+			//need to insert code for the rocket design
+			
+
+		pop();
+	}
+	this.updatePos = function(){
+		this.x += laser.x;
+		this.y += laser.y;
+	}
+}
+function mouseClicked(){
+
+	laser.push(new drawLaser(cX, mouseX, cY, mouseY));
 
 }
